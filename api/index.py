1a1ed500fd8e03d57ba438c475bd640fd4f35cd9
Mateus Duarte Gomes <47991603+MateusDuarteG@users.noreply.github.com>
2026-02-22 17:48:31 -0300

    Configure as configurações de email no aplicativo Flask
    Configuração de e-mail adicionada usando variáveis de ambiente.

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Configurações de e-mail usando variáveis de ambiente
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
-@app.route('/about')
-def about():
-    return 'About'
+# Configuração do servidor de e-mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+if __name__ == "__main__":
+    app.run(debug=True)
