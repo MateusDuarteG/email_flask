d84d857deddb198b17adb7de6b9bc3d4bd392cdb
Mateus Duarte Gomes <47991603+MateusDuarteG@users.noreply.github.com>
2026-02-25 17:06:48 -0300

    Update de credenciais
    Retornando com as credencias para o arquivo raiz para teste

--- api/index.py
+++ api/index.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_USERNAME'] = "mateuscp1708@gmail.com"
+app.config['MAIL_PASSWORD'] = "llty ouod rgfa wrmr"
+app.config['MAIL_DEFAULT_SENDER'] = "mateuscp1708@gmail.com"
 
 mail = Mail(app)
 
